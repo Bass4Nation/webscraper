637e7a40cdd33b0da32ab557ba0150c5cf0602f7
Kristoffer Snopestad Søderkvist <55276856+Bass4Nation@users.noreply.github.com>
2023-05-12 11:24:19 +0200

    Updated script to make it easier create folder and copy hooks

--- install-webscraper.js
+++ install-webscraper.js
@@ -54,10 +54,9 @@ fs.copyFileSync(serverSourcePath, serverTargetPath);
 
 console.log('Copied server.js to the target project\'s root directory.');
 
-// Copy the components, helper, and styles folders from your framework to the target project's root directory
-const foldersToCopy = ['components', 'styles', 'scrapers'];
+// Copy the components, helper, styles, and hooks folders from your framework to the target project's root directory
+const foldersToCopy = ['components', 'styles', 'scrapers', 'hooks'];
 for (const folder of foldersToCopy) {
-
   const folderSourcePath = path.join(__dirname, folder);
   const folderTargetPath = process.cwd();
 
@@ -79,40 +78,53 @@ try {
   console.error('Error installing dependencies:', error);
 }
 
-// Create public/scraped-screenshots folder if it doesn't exist
-const scrapedScreenshotsFolderPath = path.join(process.cwd(), 'public', 'scraped-screenshots');
-if (!fs.existsSync(scrapedScreenshotsFolderPath)) {
-  fs.mkdirSync(scrapedScreenshotsFolderPath, { recursive: true });
-  console.log('Created public/scraped-screenshots folder in the target project.');
-} else {
-  console.log('public/scraped-screenshots folder already exists in the target project.');
-}
+const foldersToCreate = ['public/scraped-json', 'public/scraped-txt', 'public/scraped-screenshots'];
 
-// Create public/scraped-pdfs folder if it doesn't exist
-const scrapedPdfsFolderPath = path.join(process.cwd(), 'public', 'scraped-pdfs');
-if (!fs.existsSync(scrapedPdfsFolderPath)) {
-  fs.mkdirSync(scrapedPdfsFolderPath, { recursive: true });
-  console.log('Created public/scraped-pdfs folder in the target project.');
-} else {
-  console.log('public/scraped-pdfs folder already exists in the target project.');
-}
-
-// Create public/scraped-pdfs folder if it doesn't exist
-const scrapedJsonFolderPath = path.join(process.cwd(), 'public', 'scraped-json');
-if (!fs.existsSync(scrapedJsonFolderPath)) {
-  fs.mkdirSync(scrapedJsonFolderPath, { recursive: true });
-  console.log('Created public/scraped-json folder in the target project.');
-} else {
-  console.log('public/scraped-json folder already exists in the target project.');
+for (const folder of foldersToCreate) {
+  const folderPath = path.join(process.cwd(), folder);
+  if (!fs.existsSync(folderPath)) {
+    fs.mkdirSync(folderPath, { recursive: true });
+    console.log(`Created ${folder} folder in the target project.`);
+  } else {
+    console.log(`${folder} folder already exists in the target project.`);
+  }
 }
 
 
-// Create public/scraped-pdfs folder if it doesn't exist
-const scrapedTxtFolderPath = path.join(process.cwd(), 'public', 'scraped-txt');
-if (!fs.existsSync(scrapedTxtFolderPath)) {
-  fs.mkdirSync(scrapedTxtFolderPath, { recursive: true });
-  console.log('Created public/scraped-txts folder in the target project.');
-} else {
-  console.log('public/scraped-txt folder already exists in the target project.');
-}
+// // Create public/scraped-screenshots folder if it doesn't exist
+// const scrapedScreenshotsFolderPath = path.join(process.cwd(), 'public', 'scraped-screenshots', 'public/scraped-pdfs');
+// if (!fs.existsSync(scrapedScreenshotsFolderPath)) {
+//   fs.mkdirSync(scrapedScreenshotsFolderPath, { recursive: true });
+//   console.log('Created public/scraped-screenshots folder in the target project.');
+// } else {
+//   console.log('public/scraped-screenshots folder already exists in the target project.');
+// }
+
+// // Create public/scraped-pdfs folder if it doesn't exist
+// const scrapedPdfsFolderPath = path.join(process.cwd(), 'public', 'scraped-pdfs');
+// if (!fs.existsSync(scrapedPdfsFolderPath)) {
+//   fs.mkdirSync(scrapedPdfsFolderPath, { recursive: true });
+//   console.log('Created public/scraped-pdfs folder in the target project.');
+// } else {
+//   console.log('public/scraped-pdfs folder already exists in the target project.');
+// }
+
+// // Create public/scraped-pdfs folder if it doesn't exist
+// const scrapedJsonFolderPath = path.join(process.cwd(), 'public', 'scraped-json');
+// if (!fs.existsSync(scrapedJsonFolderPath)) {
+//   fs.mkdirSync(scrapedJsonFolderPath, { recursive: true });
+//   console.log('Created public/scraped-json folder in the target project.');
+// } else {
+//   console.log('public/scraped-json folder already exists in the target project.');
+// }
+
+
+// // Create public/scraped-pdfs folder if it doesn't exist
+// const scrapedTxtFolderPath = path.join(process.cwd(), 'public', 'scraped-txt');
+// if (!fs.existsSync(scrapedTxtFolderPath)) {
+//   fs.mkdirSync(scrapedTxtFolderPath, { recursive: true });
+//   console.log('Created public/scraped-txts folder in the target project.');
+// } else {
+//   console.log('public/scraped-txt folder already exists in the target project.');
+// }
 
